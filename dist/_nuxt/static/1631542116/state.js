f051e324d0ac41227d4953fbc7e793cf00ee2b0f
Anthony Carriedo <anthonycarriedo2013@gmail.com>
2021-09-13 10:15:21 -0400

    v001

--- dist/_nuxt/static/1631542116/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631542116",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f2dd157a"}}}}(null,"\u002F"));
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  ssr: true,
+  // ssr: true,
   /* router: {
     base: '/gh-pages/'
   }, */
