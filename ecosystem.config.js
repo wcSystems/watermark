7461c88909bbfbdf24041230d827c4e64c211ad2
Anthony Carriedo <anthonycarriedo2013@gmail.com>
2021-09-12 20:20:00 -0400

    modificaciones v4

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'NuxtAppName',
+      name: 'WaterMarkNuxt',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
