39f5e1ec45839142f5becb817a0a346952006ed4
Anthony Carriedo <anthonycarriedo2013@gmail.com>
2021-09-13 09:38:58 -0400

    rev 14

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    // '@nuxtjs/pwa',
+    '@nuxtjs/pwa',
     '@nuxtjs/eslint-module',
   ],
 
