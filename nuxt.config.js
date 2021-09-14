329276a6ad481820aba4e40702acb1a5f56b538e
Anthony Carriedo <anthonycarriedo2013@gmail.com>
2021-09-14 09:15:28 -0400

    v51

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,6 @@
 export default {
 
-  // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  /* router: {
-    base: '/gh-pages/'
-  }, */
-  // Global page headers: https://go.nuxtjs.dev/config-head
 
   head: {
     title: 'water-mark-willinthon',
@@ -20,28 +15,21 @@ export default {
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/scss/main.scss'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     "@/plugins/fontawesome",
     { src: "@/plugins/watermark", mode: "client" }
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/pwa',
     '@nuxtjs/eslint-module',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     'bootstrap-vue/nuxt',
     '@nuxtjs/axios',
@@ -49,6 +37,7 @@ export default {
     '@nuxt/content',
     '@nuxtjs/auth-next'
   ],
+
   auth: {
     strategies: {
       local: {
@@ -64,10 +53,8 @@ export default {
     }
   },
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       name: "Watermark Willinthon",
@@ -83,19 +70,17 @@ export default {
     },
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     babel: {
       compact: true
     }
   },
 
-  /* server: {
-    port: 3000,
-    host: "0.0.0.0" //  on production this must be 0.0.0.0
-  }, */
+  // server: {
+  //   port: 3000,
+  //   host: "0.0.0.0" //  on production this must be 0.0.0.0
+  // },
 
 }
